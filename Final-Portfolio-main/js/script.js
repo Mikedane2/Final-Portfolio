15b32c004a15d0009482f362cd037555607f8b50
victor onyango <victor321kowalski@gmail.com>
2024-03-10 11:23:46 +0300

    version 1

---
+++ Final-Portfolio-main/js/script.js
@@ -0,0 +1,31 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings:[",","Web Developer","Web Designer","Graphics Designer","YouTuber"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+
+
+// document.getElementById("myform").addEventListener("submit", function(event) {
+//     event.preventDefault(); // Prevent default form submission
+
+//     // Get input values
+//     var name = document.getElementById("name").value;
+//     var email = document.getElementById("email").value;
+//     var email = document.getElementById("subject").value;
+//     var email = document.getElementById("message").value;
+
+//     // Do something with the input values, such as logging or validation
+//     console.log("Name:", name);
+//     console.log("Email:", email);
+//     console.log("Subject:", subject);
+//     console.log("Message:", message);
+
+//     // Optionally, clear the form fields after submission
+//     document.getElementById("name").value = "";
+//     document.getElementById("email").value = "";
+//     document.getElementById("subject").value = "";
+//     document.getElementById("message").value = "";
+// });
+
